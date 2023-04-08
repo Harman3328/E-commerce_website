0a3aca072e376064502c134b2efa5e3db168782f
Harman3328 <harmanmangat@hotmail.com>
2023-04-08 16:01:33 -0400

    added order page
    displays everything the user ordered for one order number

--- react-node-app/client/src/components/Order.js
+++ react-node-app/client/src/components/Order.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import './OrderPage.css'
+import ListGroup from 'react-bootstrap/ListGroup';
+import PropTypes from 'prop-types';
 
 const api = axios.create({
     baseURL: 'http://localhost:3001',
@@ -10,8 +12,11 @@ const api = axios.create({
 });
 
 function OrderPage() {
-    const { orderNumber } = useParams();
+    const {orderNumber} = useParams();
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [myOrders, setMyOrders] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -43,9 +48,66 @@ function OrderPage() {
         }
         checkLogin();
     }, []);
-    console.log(orderNumber)
+
+    useEffect(() => {
+        async function getOrders() {
+            setIsLoading(true);
+            try {
+                if (isLoggedIn) {
+                    const response = await api.get(`/order/${orderNumber}`);
+                    setMyOrders(response.data.info);
+                } else {
+                    setMyOrders([]);
+                }
+            } catch (error) {
+                console.error(error);
+                setError('Failed to fetch orders. Please try again later.');
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        getOrders();
+    }, [isLoggedIn]);
+
+
+    function handleClick(productCode) {
+        navigate(`/productpage/${productCode}`);
+    }
+
+    function OrderRow({ order }) {
+        return (
+            <ListGroup as="ol">
+                <ListGroup.Item as="li" onClick={() => handleClick(order.productCode)}>
+                    <div className="orderNumber">{order.productName}</div>
+                    <div className="status">{`Quantity: ${order.quantityOrdered}`}</div>
+                </ListGroup.Item>
+            </ListGroup>
+        );
+    }
+
+    OrderRow.propTypes = {
+        order: PropTypes.object.isRequired,
+    };
+
+
+    if (isLoading) {
+        return <h2>Loading</h2>;
+    }
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
+    if (myOrders.length === 0) {
+        return <h3>No Orders</h3>;
+    }
+
     return (
-        <h3>here</h3>
+        <>
+            {myOrders.map((order, index) => (
+                <OrderRow key={index} order={order} />
+            ))}
+        </>
     );
 }
 
