ef51a3d8accbbdf58745affa534d976e964c5d63
Harman3328 <harmanmangat@hotmail.com>
2023-04-24 20:38:47 -0400

    added form to product page

--- react-node-app/client/src/components/ProductPage.js
+++ react-node-app/client/src/components/ProductPage.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './ProductPage.css'
+import './ProductPage.css';
+import { checkLogin } from "./Auth";
+import { getRole } from "./Role";
 
 function ProductPage() {
     const { id } = useParams();
     const [data, setData] = useState([]);
     const [error, setError] = useState(null);
+    const [quantity, setQuanity] = useState(0);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [role, setRole] = useState()
+    const navigate = useNavigate();
 
     useEffect(() => {
         async function fetchData() {
             try {
+                const loggedIn = await checkLogin(); 
+                setIsLoggedIn(loggedIn)
+                const permission = await getRole();
+                setRole(permission)
                 const response = await axios.post(`http://localhost:3001/product/${id}`);
                 setData(response.data);
             } catch (error) {
@@ -21,9 +31,23 @@ function ProductPage() {
         fetchData();
     }, [id]);
 
+    function handleQuanityChange(event) {
+        setQuanity(event.target.value)
+    }
+
+    function handleSumbit() {
+        if (!isLoggedIn) {
+            navigate("/loginpage")
+        } else if (role === "admin") {
+            alert("Admins can't place orders")
+        } else {
+            console.log("pass")
+        }
+    }
+
     const renderProductPage = () => {
         const [productData] = data;
-        const headers = Object.keys(productData).filter(header => header !== "productCode");
+        const headers = Object.keys(productData).filter(header => header !== "productCode" && header !== "image");
         return (
             <div key={id} className="product-info">
                 <h1 className="product-title">{productData.productName}</h1>
@@ -43,6 +67,12 @@ function ProductPage() {
     return (
         <div className="product-page">
             {data.length > 0 ? renderProductPage() : <h1>Loading...</h1>}
+            <div className="product-page-form">
+                <form onSubmit={handleSumbit}>
+                    <input type="text" className="product-page-quanity-input" value={quantity} onChange={handleQuanityChange}></input>
+                    <button type="submit" className="product-page-button">Add</button>
+                </form>
+            </div>
         </div>
     );
 }
